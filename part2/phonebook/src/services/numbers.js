0fdd9d786f69c5e6d649cc31a9663d42b15ce85a
asianomainen <oskari.nuottonen@gmail.com>
2022-04-11 00:24:14 +0300

    Exercises 2.19-2.20

--- part2/phonebook/src/services/numbers.js
+++ part2/phonebook/src/services/numbers.js
@@ -11,7 +11,7 @@ const getAll = () => {
 }
 
 const deleteNumber = (id) => {
-    axios.delete(`${baseUrl}/${id}`)
+    return axios.delete(`${baseUrl}/${id}`)
 }
 
 const update = (id, newObject) => {

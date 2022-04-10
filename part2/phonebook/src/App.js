9bb89172429e0c2b820eea7f2ef322fb976cc730
asianomainen <oskari.nuottonen@gmail.com>
2022-04-11 00:24:14 +0300

    Exercises 2.19-2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,19 +3,22 @@ import Filter from "./components/Filter";
 import AddPersonForm from "./components/AddPersonForm";
 import Numbers from "./components/Numbers";
 import numberService from "./services/numbers"
+import Notification from "./components/Notification";
+import Error from "./components/Error";
 
 const App = () => {
     const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState("")
+    const [newNumber, setNewNumber] = useState("")
+    const [newSearch, setNewSearch] = useState("")
+    const [notification, setNotification] = useState(null)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         numberService
             .getAll()
             .then(numbers => setPersons(numbers))
     }, [])
-
-    const [newName, setNewName] = useState("")
-    const [newNumber, setNewNumber] = useState("")
-    const [newSearch, setNewSearch] = useState("")
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -32,7 +35,14 @@ const App = () => {
                 const changedContact = {...contact, number: personObj.number}
                 numberService
                     .update(contact.id, changedContact)
-                    .then(returnedPerson => setPersons(persons.map(person => person.id === contact.id ? returnedPerson : person)))
+                    .then(returnedPerson => {
+                        setPersons(persons.map(person => person.id === contact.id ? returnedPerson : person))
+
+                        setNotification(`Number for ${contact.name} was updated`)
+                        setTimeout(() => {
+                            setNotification(null)
+                        }, 2000)
+                    })
             }
         } else {
             numberService
@@ -44,6 +54,21 @@ const App = () => {
 
         setNewName("")
         setNewNumber("")
+    }
+
+    const deletePerson = (deletedPerson) => {
+        if (window.confirm(`Delete ${deletedPerson.name}?`)) {
+            numberService
+                .deleteNumber(deletedPerson.id)
+                .catch(() => {
+                    setError(`Information of ${deletedPerson.name} has already been removed from the server`)
+                    setTimeout(() => {
+                        setError(null)
+                    }, 2000)
+                })
+
+            setPersons(persons.filter(person => person.id !== deletedPerson.id))
+        }
     }
 
     const handleAddName = (event) => {
@@ -58,18 +83,11 @@ const App = () => {
         setNewSearch(event.target.value)
     }
 
-    const deletePerson = (deletedPerson) => {
-        if (window.confirm(`Delete ${deletedPerson.name}?`)) {
-            numberService
-                .deleteNumber(deletedPerson.id)
-
-            setPersons(persons.filter(person => person.id !== deletedPerson.id))
-        }
-    }
-
     return (
         <div>
             <h2>Phonebook</h2>
+            <Notification message={notification}/>
+            <Error message={error}/>
             <Filter handler={handleSearch}/>
 
             <h2>Add new person</h2>

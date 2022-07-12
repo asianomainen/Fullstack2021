7204154aac2c49182e6471c881c49a0d0e52578e
asianomainen <oskari.nuottonen@gmail.com>
2022-07-12 19:52:19 +0300

    Exercises 4.8-4.12

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    if (body.title === undefined || body.url === undefined) {
+        response.status(400).json()
+    }
+
+    body.likes = body.likes || 0
+
+    const blog = new Blog(body)
+
+    await blog.save()
+    response.status(201).json(blog)
 })
 
 module.exports = blogsRouter
--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,67 +1,4 @@
 const listHelper = require('../utils/list_helper')
-const {mostLikes} = require("../utils/list_helper");
-
-const listWithOneBlog = [
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-    }
-]
-
-const listWithSeveralBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    },
-    {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: "5a422b891b54a676234d17fa",
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10,
-        __v: 0
-    },
-    {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0,
-        __v: 0
-    },
-    {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-        __v: 0
-    }
-]
 
 test('dummy returns one', () => {
     const blogs = []
@@ -76,12 +13,12 @@ describe('total likes', () => {
     })
 
     test('when list has only one blog, equals the likes of that', () => {
-        const result = listHelper.totalLikes(listWithOneBlog)
+        const result = listHelper.totalLikes(listHelper.listWithOneBlog)
         expect(result).toBe(5)
     })
 
     test('of a bigger list is calculated right', () => {
-        const result = listHelper.totalLikes(listWithSeveralBlogs)
+        const result = listHelper.totalLikes(listHelper.listWithSeveralBlogs)
         expect(result).toBe(36)
     })
 
@@ -89,7 +26,7 @@ describe('total likes', () => {
 
 describe('favorite blog', () => {
     test('is returned when there is one blog', () => {
-        const result = listHelper.favoriteBlog(listWithOneBlog)
+        const result = listHelper.favoriteBlog(listHelper.listWithOneBlog)
         const expected = {
             title: 'Go To Statement Considered Harmful',
             author: 'Edsger W. Dijkstra',
@@ -100,7 +37,7 @@ describe('favorite blog', () => {
     })
 
     test('is returned when there are several blogs', () => {
-        const result = listHelper.favoriteBlog(listWithSeveralBlogs)
+        const result = listHelper.favoriteBlog(listHelper.listWithSeveralBlogs)
         const expected = {
             title: "Canonical string reduction",
             author: "Edsger W. Dijkstra",
@@ -119,11 +56,11 @@ describe('favorite blog', () => {
 
 describe('most blogs', () => {
     test('is returned when there is one blog', () => {
-        expect(listHelper.mostBlogs(listWithOneBlog)).toEqual({"author": "Edsger W. Dijkstra", "blogs": 1})
+        expect(listHelper.mostBlogs(listHelper.listWithOneBlog)).toEqual({"author": "Edsger W. Dijkstra", "blogs": 1})
     })
 
     test('is returned when there are several blogs', () => {
-        expect(listHelper.mostBlogs(listWithSeveralBlogs)).toEqual({"author": "Robert C. Martin", "blogs": 3})
+        expect(listHelper.mostBlogs(listHelper.listWithSeveralBlogs)).toEqual({"author": "Robert C. Martin", "blogs": 3})
     })
 
     test('is returned when there are no blogs', () => {
@@ -135,11 +72,11 @@ describe('most blogs', () => {
 
 describe('most likes', () => {
     test('is returned when there is one blog', () => {
-        expect(mostLikes(listWithOneBlog)).toEqual({"author": "Edsger W. Dijkstra", "likes": 5})
+        expect(listHelper.mostLikes(listHelper.listWithOneBlog)).toEqual({"author": "Edsger W. Dijkstra", "likes": 5})
     })
 
     test('is returned when there are several blogs', () => {
-        expect(mostLikes(listWithSeveralBlogs)).toEqual({"author": "Edsger W. Dijkstra", "likes": 17})
+        expect(listHelper.mostLikes(listHelper.listWithSeveralBlogs)).toEqual({"author": "Edsger W. Dijkstra", "likes": 17})
     })
 
     test('is returned when there are no blogs', () => {

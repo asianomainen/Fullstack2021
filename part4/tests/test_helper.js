8e06fbd943c87e6822efdbbcf493759fd1a3299b
asianomainen <oskari.nuottonen@gmail.com>
2022-07-18 19:08:04 +0300

    Remove commented lines

--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -17,42 +17,36 @@ const listWithSeveralBlogs = [
         author: "Michael Chan",
         url: "https://reactpatterns.com/",
         likes: 7,
-        //user: 'asdfadsf234234asd'
     },
     {
         title: "Go To Statement Considered Harmful",
         author: "Edsger W. Dijkstra",
         url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
         likes: 5,
-        //user: 'asthrdty5t4334434'
     },
     {
         title: "Canonical string reduction",
         author: "Edsger W. Dijkstra",
         url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
         likes: 12,
-        //user: 'brefay65yrg434432'
     },
     {
         title: "First class tests",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
         likes: 10,
-        //user: 'dgrdutyjrh6554gr2'
     },
     {
         title: "TDD harms architecture",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
         likes: 0,
-        //user: 'dgrdutyjrh6554gr2'
     },
     {
         title: "Type wars",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
         likes: 2,
-        //user: 'asthrdty5t4334434'
     }
 ]
 

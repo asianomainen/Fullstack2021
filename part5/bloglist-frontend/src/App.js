4ed87ef1b2f7dbfaf3f62c4329461ad10ad5fe1a
asianomainen <oskari.nuottonen@gmail.com>
2022-07-20 17:10:08 +0300

    Exercises 5.1-5.12

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,195 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import NewBlogForm from './components/NewBlogForm'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [notification, setNotification] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const allBlogs = await blogService.getAll()
+      allBlogs.sort((a, b) => b.likes - a.likes)
+      setBlogs(allBlogs)
+    }
+
+    fetchBlogs()
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const notify = (message, type = 'info') => {
+    setNotification({ message, type })
+    setTimeout(() => {
+      setNotification(null)
+    }, 3000)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      notify('Wrong credentials', 'alert')
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
+
+  const handleAddBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = await blogService.create({
+        title, author, url
+      })
+
+      const loggedUser = JSON.parse(window.localStorage.getItem('loggedUser'))
+
+      newBlog.user = {
+        username: loggedUser.username,
+        name: loggedUser.name
+      }
+
+      setBlogs(blogs.concat(newBlog))
+      notify(`a new blog ${title} by ${author} added`)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (e) {
+      notify('Something went wrong with adding the blog', 'alert')
+    }
+  }
+
+  const handleDeleteBlog = async (event, blog) => {
+    event.preventDefault()
+
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      try {
+        await blogService.deleteBlog(blog.id)
+        notify(`${blog.title} by ${blog.author} deleted`)
+        setBlogs(blogs.filter(b => b.id !== blog.id))
+      } catch (e) {
+        notify('Something went wrong with deleting the blog', 'alert')
+      }
+    }
+  }
+
+  const handleLike = async (event, blog) => {
+    event.preventDefault()
+
+    try {
+      const updatedBlog = await blogService.update(blog.id, {
+        user: blog.user.id,
+        likes: blog.likes + 1,
+        author: blog.author,
+        title: blog.title,
+        url: blog.url
+      })
+
+      updatedBlog.user = {
+        username: blog.user.username,
+        name: blog.user.name
+      }
+
+      const updatedBlogs = blogs
+        .map(b => b.id !== blog.id ? b : updatedBlog)
+        .sort((a, b) => b.likes - a.likes)
+
+      setBlogs(updatedBlogs)
+    } catch (e) {
+      notify('Something went wrong with liking the blog', 'alert')
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <div>
+        <LoginForm
+          setUsername={setUsername}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+          notification={notification}
+          username={username}
+          password={password}
+        />
+      </div>
+    )
+  }
+
+  const blogForm = () => {
+    return (
+      <div>
+        <h2>Blogs</h2>
+        <Notification notification={notification} />
+        <div>
+          {user.name} is logged in
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+        <br />
+        <Togglable buttonLabel='New blog'>
+          <NewBlogForm
+            handleAddBlog={handleAddBlog}
+            setTitle={setTitle}
+            setAuthor={setAuthor}
+            setUrl={setUrl}
+            title={title}
+            author={author}
+            url={url}
+          />
+        </Togglable>
+        <br />
+        <div>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} handleLike={handleLike} handleDelete={handleDeleteBlog} />
+          )}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {user === null ?
+        loginForm() :
+        blogForm()
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const Blog = ({ handleLike, handleDelete, blog }) => {
+  const [viewDetailsVisible, setViewDetailsVisible] = useState(false)
+  const hideWhenVisible = { display: viewDetailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: viewDetailsVisible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setViewDetailsVisible(!viewDetailsVisible)
+  }
+
+  const loggedUser = JSON.parse(window.localStorage.getItem('loggedUser'))
+  const correctUser = (loggedUser.username === blog.user.username)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button style={hideWhenVisible} onClick={toggleVisibility}>View</button>
+        <button style={showWhenVisible} onClick={toggleVisibility}>Hide</button>
+        <div style={showWhenVisible} >
+          {blog.url} <br />
+          likes {blog.likes} <button onClick={(event) => handleLike(event, blog)}>Like</button> <br />
+          {blog.user.name} <br />
+          {correctUser && <button onClick={(event) => handleDelete(event, blog)}>Remove</button>}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog

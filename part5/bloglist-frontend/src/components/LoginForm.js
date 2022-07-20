6b462350715d4a25bf25e4f6d80babb22c0bbaa5
asianomainen <oskari.nuottonen@gmail.com>
2022-07-20 17:10:08 +0300

    Exercises 5.1-5.12

---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import Notification from './Notification'
+import PropTypes from 'prop-types'
+import Togglable from './Togglable'
+
+const LoginForm = ({
+  setUsername,
+  setPassword,
+  handleLogin,
+  notification,
+  username,
+  password
+}) => {
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <Notification notification={notification} />
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
+
+Togglable.displayName = 'Togglable'
+
+export default LoginForm

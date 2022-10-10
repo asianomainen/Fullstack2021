c1d7227cae859b937314d1c20d30ad13c2c34609
asianomainen <oskari.nuottonen@gmail.com>
2022-10-10 15:40:28 +0300

    Exercises 9.4-9.5

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -17,7 +17,7 @@ const parseValues = (args: Array<string>): BmiValues => {
   }
 }
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2)
 
   if (bmi < 18.5) {
---
+++ part9/index.ts
@@ -0,0 +1,27 @@
+import express = require('express')
+import { calculateBmi } from './bmiCalculator'
+
+const app = express()
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query
+
+  if (isNaN(Number(height)) || isNaN(Number(weight))) {
+    res.send({ error: 'malformatted parameters' })
+  }
+
+  const bmi = calculateBmi(Number(height), Number(weight))
+  res.send({ weight: weight, height: height, bmi: bmi })
+})
+
+app.get('/')
+
+const PORT = 3003
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

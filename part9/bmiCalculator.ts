5dc677e15a31e0cb41a0c7ae6e158598aba9ab76
asianomainen <oskari.nuottonen@gmail.com>
2022-10-10 16:50:48 +0300

    Exercises 9.6-9.7

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,43 +1,11 @@
-interface BmiValues {
-  height: number
-  weight: number
-}
-
-const parseValues = (args: Array<string>): BmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments')
-  if (args.length > 4) throw new Error('Too many arguments')
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    }
-  } else {
-    throw new Error('Provided values were not numbers!')
-  }
-}
-
 export const calculateBmi = (height: number, weight: number): string => {
-  const bmi = weight / Math.pow(height / 100, 2)
+  const bmi = weight / Math.pow(height / 100, 2);
 
   if (bmi < 18.5) {
-    return 'Underweight'
+    return 'Underweight';
   } else if (bmi >= 18.5 && bmi < 24.9) {
-    return 'Normal (healthy weight)'
+    return 'Normal (healthy weight)';
   } else {
-    return 'Overweight'
+    return 'Overweight';
   }
-}
-
-try {
-  const { height, weight } = parseValues(process.argv)
-  console.log(calculateBmi(height, weight))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
-  }
-
-  console.log(errorMessage)
-}
+};
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,66 +1,37 @@
 interface ExreciseValues {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: number
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
-interface CalculatorArgs {
-  exercises: Array<number>
-  target: number
-}
-
-const parseExercises = (args: Array<string>): CalculatorArgs => {
-  if (args.length < 4) throw new Error('Not enough arguments')
-
-  // Remove the two first values of process.argv
-  args.shift()
-  args.shift()
-
-  let exercises: Array<number> = []
-
-  args.map((e) => {
-    if (isNaN(Number(e))) {
-      throw new Error('Provided values were not numbers!')
-    }
-    exercises.push(Number(e))
-  })
-
-  const target = exercises.shift()
-
-  return {
-    exercises,
-    target,
-  }
-}
-
-const calculateExercises = (
+export const calculateExercises = (
   args: Array<number>,
   target: number
 ): ExreciseValues => {
-  const periodLength = args.length
-  const trainingDays = args.filter((e) => e > 0).length
-  const exerciseHours = args.reduce((a, b) => a + b)
-  const average = exerciseHours / periodLength
-  const success = average >= target
-  let rating = 0
-  let ratingDescription = 'Something went wrong'
+  const periodLength = args.length;
+  const trainingDays = args.filter((e) => e > 0).length;
+  const exerciseHours = args.reduce((a, b) => a + b);
+  const average = exerciseHours / periodLength;
+  const success = average >= target;
+  let rating = 0;
+  let ratingDescription = 'Something went wrong';
 
   if (average === 0) {
-    ratingDescription = 'Try again next week!'
-    rating = 0
+    ratingDescription = 'Try again next week!';
+    rating = 0;
   } else if (average < target / 2) {
-    ratingDescription = 'You can do better!'
-    rating = 1
+    ratingDescription = 'You can do better!';
+    rating = 1;
   } else if (average >= target / 2 && average < target) {
-    ratingDescription = 'Not too bad, but you can still do better!'
-    rating = 2
+    ratingDescription = 'Not too bad, but you can still do better!';
+    rating = 2;
   } else if (average >= target) {
-    ratingDescription = 'Great job!'
-    rating = 3
+    ratingDescription = 'Great job!';
+    rating = 3;
   }
 
   return {
@@ -71,18 +42,5 @@ const calculateExercises = (
     ratingDescription,
     target,
     average,
-  }
-}
-
-try {
-  const { exercises, target } = parseExercises(process.argv)
-  console.log(calculateExercises(exercises, target))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
-  }
-
-  console.log(errorMessage)
-}
+  };
+};
